69d33f733fdc03dc14bb74cce99e879f5bfeb322
Javier Franco <javier.franco18@gmail.com>
2019-11-05 15:26:16 +0100

    Solution task manager with no refactor - pending styles

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,33 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @task = Task.create(title: params[:task][:title], details: params[:task][:details])
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(title: params[:task][:title], details: params[:task][:details])
+    redirect_to task_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+end
